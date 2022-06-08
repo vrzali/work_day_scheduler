e9d5bd80c1bb8bacabfe5571d1410aaf3765d5c9
Vadad Rzali <vedad.rzali@gmail.com>
2022-06-08 00:33:06 -0400

    added save button and used local storage

--- script.js
+++ script.js
@@ -1,13 +1,31 @@
 
 $("#currentDay").text(moment().format('dddd MMMM Do YYYY'));
 
+var saveBtn = $(".saveBtn");
+
+saveBtn.on("click", function() {
+
+    var time = $(this).siblings(".hour").text();
+    var plan = $(this).siblings(".plan").val();
+    localStorage.setItem(time, plan);
+});
+
+function usePlanner() {
+
+    $(".hour").each(function() {
+        var currHour = $(this).text();
+        var currPlan = localStorage.getItem(currHour);
+        if(currPlan !== null) {
+            $(this).siblings(".plan").val(currPlan);
+        }
+    });
+}
+
 function timeBlockColor() {
     var hour = moment().hours();
 
     $(".time-block").each(function() {
         var currHour = parseInt($(this).attr("id"));
-
-        // console.log(this); //each time-block
 
         if (currHour > hour) {
             $(this).addClass("future");
@@ -20,3 +38,4 @@ function timeBlockColor() {
 };
 
 timeBlockColor();
+usePlanner();
