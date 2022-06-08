f85eddd16f62c40e0595e180049ab3a0396f459f
Vadad Rzali <vedad.rzali@gmail.com>
2022-06-08 00:02:28 -0400

    Added colors to time blocks based on the actual time

--- script.js
+++ script.js
@@ -1,2 +1,22 @@
 
 $("#currentDay").text(moment().format('dddd MMMM Do YYYY'));
+
+function timeBlockColor() {
+    var hour = moment().hours();
+
+    $(".time-block").each(function() {
+        var currHour = parseInt($(this).attr("id"));
+
+        // console.log(this); //each time-block
+
+        if (currHour > hour) {
+            $(this).addClass("future");
+        } else if (currHour === hour) {
+            $(this).addClass("present");
+        } else {
+            $(this).addClass("past");
+        }
+    })
+};
+
+timeBlockColor();
